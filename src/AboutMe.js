7d87d0ad280114b073beee169263e1531bda0b7a
Cameron Hatch <cameronlavigne92@gmail.com>
2018-12-28 09:55:36 -0700

    added about me section

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import './aboutme.css';
 
-export default function AboutMe(props){
+export default function AboutMe(props) {
   return (
-    <div>
+    <div className='about-me'>
       <h3>About Me</h3>
-      <p>Hi! I'm Cameron.</p>
-      <p>I used to be a mathematician, but now I'm a programmer.
-        I really love problem solving, learning new things, and
-        building cool stuff.
+      <div className='info-holder'>
+        <p className='hello'>Hi! I'm Cameron.</p>
+        <p className='bio'>
+          I really love problem solving, learning new things, working
+          with awesome people,
+          and building cool stuff. I love programming because it allows
+          me to do all of those things at the same time!
+          When I'm not coding, I like to crochet, read up on real
+        analysis, or play <a href='https://www.zmangames.com/en/games/pandemic/'>Pandemic</a>.
       </p>
+      </div>
     </div>
   )
 }
