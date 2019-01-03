8d437ed6aa35b42347463bfe87030aad3564d278
Cameron Hatch <cameronlavigne92@gmail.com>
2019-01-03 15:13:18 -0700

    updated about me section

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,11 +7,20 @@ export default function AboutMe(props) {
       <h2 id='AboutMe'>About Me</h2>
       <article className='info-holder'>
         <p className='hello'>Hi! I'm Cameron.</p>
+        <img src='images/Cameron.jpg' className='my-picture'></img>
         <p className='bio'>
-          I really love problem solving, learning new things, working
-          with awesome people,
-          and building cool stuff. I love programming because it allows
-          me to do all of those things at the same time!
+          I'm a mathematician turned full-stack developer. 
+          After getting a masters degree in pure math, I wanted to do something
+           that would let me use all of the problem solving and critical thinking
+           skills I learned while studying math, but maybe something that was a little
+           more useful and employable than abstract algebra and topology. Enter programming!
+          <br/>
+          <br/>
+          What I love about coding is that it incorporates all of the things I love about
+           math, while also allowing me to build really cool things and work with a lot of awesome
+            developers. 
+            <br/>
+          <br/>   
           When I'm not coding, I like to crochet, read up on real
         analysis, or play <a href='https://www.zmangames.com/en/games/pandemic/'>Pandemic</a>.
       </p>
--- src/Contact.js
+++ src/Contact.js
@@ -5,18 +5,19 @@ export default function Contact(props) {
   return (
     <section className='contact-info' id='Contact'>
     <h2>Contact Info</h2>
+    <p className='being-nice'>I'd love to hear from you!</p>
       <ul className='contact-list'>
         <li>
-          Email: <a href='mailto:cameronlavigne92@gmail.com' className='contact-icon' title='Email cameronlavigne92@gmail.com'>
+          <a href='mailto:cameronlavigne92@gmail.com' className='contact-icon' title='Email cameronlavigne92@gmail.com'>
             <i className="fa fa-envelope"></i>
           </a>
         </li>
         <li>
-          LinkedIn: <a href='https://www.linkedin.com/in/cameron-hatch/' target='false' className='contact-icon' title='LinkedIn'>
+          <a href='https://www.linkedin.com/in/cameron-hatch/' target='false' className='contact-icon' title='LinkedIn'>
             <i className="fa fa-linkedin-square"></i></a>
         </li>
         <li>
-          GitHub: <a href='https://github.com/CameronHatch92' target='false' className='contact-icon' title='GitHub'>
+          <a href='https://github.com/CameronHatch92' target='false' className='contact-icon' title='GitHub'>
             <i className="fa fa-github-square"></i>
           </a>
         </li>
